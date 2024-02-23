860c061c612e6520dd983523029424c90e9db8f4
imLukePex <luca.pessione@gmail.com>
2024-02-23 17:35:09 +0100

    feat: added migration foreign key

--- database/seeders/TypeTableSeeder.php
+++ database/seeders/TypeTableSeeder.php
@@ -5,19 +5,17 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Type;
+
+class TypeTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this -> call([
-
-            ProjectTableSeeder :: class,
-            TypeTableSeeder :: class
-        ]);
+        Type :: factory() -> count(10) -> create();
     }
 }
